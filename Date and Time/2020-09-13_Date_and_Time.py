ac72815f44b3f1a971d88642646e7fc8aa91f60c
Cihan <cihanayana18@gmail.com>
2020-09-13 15:06:54 -0400

    2020-09-13 1459

--- Date and Time/2020-09-13_Date_and_Time.py
+++ Date and Time/2020-09-13_Date_and_Time.py
@@ -7,4 +7,16 @@ print("Today is", today)
 
 print("The date components are", today.day, today.month, today.year) #we called the day, month and year seperately.
 
-print("The weekday number is", today.weekday())
+print("The weekday number is", today.weekday()) #this method displays the current day.
+
+days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"] #this prints the days.
+for x in days:
+    print(x)
+
+print("The weekday is", days[today.weekday()]) #this prints the day.
+
+today = datetime.now() #we called the now method from datetime class of datetime module.
+print("The current time is", today)
+
+t = datetime.time(datetime.now()) #this only will display the time not the date
+print(t)
