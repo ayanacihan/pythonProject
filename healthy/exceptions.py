59008180c178f421aec1bd3eb6f7f1ea640cfa79
Cihan <cihanayana18@gmail.com>
2020-09-17 22:07:49 -0400

    2020-09-17 2155

---
+++ healthy/exceptions.py
@@ -0,0 +1,6 @@
+#This is one example of an exception. dividing the number with 0
+'''6/0
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+ZeroDivisionError: division by zero'''
+
